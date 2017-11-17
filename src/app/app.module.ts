879df960596a8fe42cd6616ed6ad732d612a47ac
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-11-17 21:47:10 +0100

    Title casing using custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { AuthorsService } from './authors.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { TitlecasingComponent } from './titlecasing/titlecasing.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorsComponent,
     FavoriteComponent,
-    TitlecasingComponent
+    TitlecasingComponent,
+    TitleCasePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     AuthorsService
